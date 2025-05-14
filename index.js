fbdf12ab1d4ef6d4829e15532eb2e05a5b02f3ce
freegoatdev <dev@freegoat.io>
2025-05-14 19:13:35 -0300

    🔥 FREEGOAT backend inicial

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Backend Node.js do FREEGOAT
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('🚀 FREEGOAT backend está online!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
